c2ba668450c964c38186bede26aff70df4a9574e
Ty Richards <me@tyrichards.com>
2014-05-23 17:20:13 -0500

    complete menu

--- wp-content/themes/Paradox/page-menu.php
+++ wp-content/themes/Paradox/page-menu.php
@@ -16,7 +16,7 @@ Template Name: Menu
 	</div>				
 </section>
 
-<section class="primary-content">       
+<section class="section primary-content">       
 	<section class="main-col page-content">                
 		<div id="main" class="site-main" role="main">
 
@@ -33,38 +33,50 @@ Template Name: Menu
 							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu/wings.jpg" class="img-responsive img-circle" alt="Bronson Rock - Wings">
 						</div>
 						<div class="col-md-4">							
-							<h3>Potato Skins   $9.29</h3>
-							Fried skins topped with mixed cheese &amp; bacon. Served with sour cream.
-							<h3>Flat Bread Margarita Pizza   $8.29</h3>
-							Wood fired flat bread, topped with marinara, roasted tomatoes, fresh basil, blended mozzarella cheese, drizzled with a sweet basalmic glaze.
-							<h3>Loaded Nachos  $8.99</h3>
-							Chips, queso, beans, lettuce, tomato, onions, guacamole, sour cream &amp; pickled jalapenos.
-							Ground beef or grilled chicken can be added for $1.99
+							<ul>
+								<li>
+									<h3>Potato Skins   $9.29</h3>
+									<p>Fried skins topped with mixed cheese &amp; bacon. Served with sour cream.</p>
+								</li>
+								<li>
+									<h3>Flat Bread Margarita Pizza   $8.29</h3>
+									<p>Wood fired flat bread, topped with marinara, roasted tomatoes, fresh basil, blended mozzarella cheese, drizzled with a sweet basalmic glaze.</p>
+								</li>
+								<li>
+									<h3>Loaded Nachos  $8.99</h3>
+									<p>Chips, queso, beans, lettuce, tomato, onions, guacamole, sour cream &amp; pickled jalapenos.<br>
+									Ground beef or grilled chicken can be added for $1.99</p>
+								</li>
+								<li>
+									<h3>Wings (one pound)   $8.99</h3>
+									<p>Lemon Pepper, BBQ, Teriyaki, Garlic, Parmesan, Buffalo (mild, hot, extra hot), Sweet Chili</p>							
+								</li>
+							</ul>
 						</div>						
 						<div class="col-md-4">
-							<h3>Wings (one pound)   $8.99</h3>
-							Lemon Pepper, BBQ, Teriyaki, Garlic, Parmesan, Buffalo (mild, hot, extra hot), Sweet Chili
-							<h3>Fried Mozzarella  $8.29</h3>
-							<h3>Jalapeño Poppers  $7.99</h3>
-							<h3>Basket of Fries  $3.99</h3>
-							<h3>Beer-Battered Onion Rings  $6.99</h3>
-							<h3>Tempura-Battered Mushrooms  $7.29</h3>
-							<h3>Chips &amp; Fire-Roasted Salsa  $5.99</h3>
-							Add queso $2.49
-							<h3>Locked &amp; Loaded Fries   $7.99</h3>
-							<h3>Queso Cheese, Bacon Crumbles, &amp; Pickled Jalapenos</h3>
-							Served with ranch dressing.
+							<ul>
+								<li><h3>Fried Mozzarella  $8.29</h3></li>
+								<li><h3>Jalapeño Poppers  $7.99</h3></li>
+								<li><h3>Basket of Fries  $3.99</h3></li>
+								<li><h3>Beer-Battered Onion Rings  $6.99</h3></li>
+								<li><h3>Tempura-Battered Mushrooms  $7.29</h3></li>
+								<li><h3>Chips &amp; Fire-Roasted Salsa  $5.99</h3></li>
+								<li><p>Add queso $2.49</p></li>
+								<li><h3>Locked &amp; Loaded Fries  $7.99</h3></li>
+								<li><h3>Queso Cheese, Bacon Crumbles, &amp; Pickled Jalapenos</h3></li>
+								<li><p>Served with ranch dressing.</p></li>
+							</ul>
 						</div>
 					</div> <!-- .row -->
 				</div> <!-- .container -->
 			</section> 
 
-			<section class="menu bg-secondary">
+			<section class="section menu bg-primary">
 				<div class="container">
 					<div class="row text-center">
 						<div class="col-md-12">
 							<h2>Wood-Fired Burgers</h2>
-							Handmade burgers seasoned &amp; grilled over a wood flame &amp; slapped on a toasted poppy seed bun. Served with a handful of fries (or upgrade to Onion Rings, Locked &amp; Loaded fries, or Salad)
+							<p>Handmade burgers seasoned &amp; grilled over a wood flame &amp; slapped on a toasted poppy seed bun. Served with a handful of fries (or upgrade to Onion Rings, Locked &amp; Loaded fries, or Salad)</p>
 						</div>
 					</div>
 					<div class="row">	
@@ -72,66 +84,108 @@ Template Name: Menu
 							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu/burger.jpg" class="img-responsive img-circle" alt="Bronson Rock - Wings">
 						</div>							
 						<div class="col-md-4">
-							<h3>The Bronson  $9.99</h3>
-							Cheddar cheese, smoked bacon, grilled onions &amp; jalapenos. LTP w/house dressing
-							<h3>Classic Rock Burger   $8.49</h3>
-							LTOP with mustard
-							<h3>Pig Sleeping on a Rock $9.99</h3>
-							Cheddar cheese &amp; smoked bacon, LTOP, w/mayo &amp; mustard
-							<h3>The Hendrix  $8.99</h3>
-							Swiss cheese, grilled mushrooms, LTO w/mayo
-							<h3>The Duke  $9.99</h3>
-							Pepper Jack cheese, guacamole &amp; smoked bacon, LTO w/mayo
-							<h3>Bleu’s Burger   $8.99</h3>
-							Bleu cheese crumbles, tangy BBQ sauce, LTOP w/mayo
+							<ul>
+								<li>
+									<h3>The Bronson  $9.99</h3>
+									<p>Cheddar cheese, smoked bacon, grilled onions &amp; jalapenos. LTP w/house dressing</p>
+								</li>
+								<li>
+									<h3>Classic Rock Burger   $8.49</h3>
+									<p>LTOP with mustard</p>
+								</li>
+								<li>
+									<h3>Pig Sleeping on a Rock $9.99</h3>
+									<p>Cheddar cheese &amp; smoked bacon, LTOP, w/mayo &amp; mustard</p>
+								</li>
+								<li>
+									<h3>The Hendrix  $8.99</h3>
+									<p>Swiss cheese, grilled mushrooms, LTO w/mayo</p>
+								</li>
+								<li>
+									<h3>The Duke  $9.99</h3>
+									<p>Pepper Jack cheese, guacamole &amp; smoked bacon, LTO w/mayo</p>
+								</li>
+								<li>
+									<h3>Bleu’s Burger   $8.99</h3>
+									<p>Bleu cheese crumbles, tangy BBQ sauce, LTOP w/mayo</p>
+								</li>
 						</div>
 						<div class="col-md-4">
-							<h3>Hippie Hallow (Veggie Burger)  $8.29</h3>
-							Pepper Jack cheese, roasted peppers, sautéed mushrooms, LTOP w/chipotle mayo
-							*we also offer a Portabello mushroom substitute
-							<h3>Hawaiian Burger   $9.99</h3>
-							Smoked bacon, grilled pineapple, LTO w/tangy chili sauce &amp; mayo
-							<h3>The Barnyard Brawl  $9.99</h3>
-							American cheese, smoked bacon, hash browns and a fried egg, LTO w/mayo
-							<h3>The Roadie  $9.99</h3>
-							Cheddar cheese, smoked bacon, fried onions, tangy BBQ sauce, LTO w/mayo
+							<ul>
+								<li>
+									<h3>Hippie Hallow (Veggie Burger)  $8.29</h3>
+									<p>Pepper Jack cheese, roasted peppers, sautéed mushrooms, LTOP w/chipotle mayo
+									*we also offer a Portabello mushroom substitute</p>
+								</li>
+								<li>
+									<h3>Hawaiian Burger   $9.99</h3>
+									<p>Smoked bacon, grilled pineapple, LTO w/tangy chili sauce &amp; mayo</p>
+								</li>
+								<li>
+									<h3>The Barnyard Brawl  $9.99</h3>
+									<p>American cheese, smoked bacon, hash browns and a fried egg, LTO w/mayo</p>
+								</li>
+								<li>
+									<h3>The Roadie  $9.99</h3>
+									<p>Cheddar cheese, smoked bacon, fried onions, tangy BBQ sauce, LTO w/mayo</p>
+								</li>
 						</div>
 					</div> <!-- .row -->
 				</div> <!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu">
 				<div class="container">
 					<div class="row">				
 						<div class="col-md-6">	
-							<h2>Sandwiches</h2>			
-							<h3>The Yardbird  $8.99</h3>
-							Seasoned &amp; grilled chicken, swiss cheese, LTOP w/chipotle mayo
-							<h3>Rockin’ Reuben  $8.29</h3>
-							Pastrami, sour kraut, swiss, thousand island, all melted on marble rye
-							<h3>Acoustic Act  $8.99</h3>
-							Tender sliced turkey, smoked bacon, swiss, lettuce, tomato, w/avocado mayo on marble rye.
-							<h3>French Dip   $9.99</h3>
-							Thinly sliced Rib-eye steak, seasoned &amp; grilled. Topped with swiss cheese on a toasted poppy seed roll w/au jus dipping sauce
-							<h3>Fish Sandwich   $8.99</h3>
-							Blackened tilapia, LTOP w/chipotle mayo on poppy seed bun
+							<h2>Sandwiches</h2>		
+							<ul>
+								<li>	
+									<h3>The Yardbird  $8.99</h3>
+									<p>Seasoned &amp; grilled chicken, swiss cheese, LTOP w/chipotle mayo</p>
+								</li>
+								<li>
+									<h3>Rockin’ Reuben  $8.29</h3>
+									<p>Pastrami, sour kraut, swiss, thousand island, all melted on marble rye</p>
+								</li>
+								<li>
+									<h3>Acoustic Act  $8.99</h3>
+									<p>Tender sliced turkey, smoked bacon, swiss, lettuce, tomato, w/avocado mayo on marble rye.</p>
+								</li>
+								<li>
+									<h3>French Dip   $9.99</h3>
+									<p>Thinly sliced Rib-eye steak, seasoned &amp; grilled. Topped with swiss cheese on a toasted poppy seed roll w/au jus dipping sauce</p>
+								</li>
+								<li>
+									<h3>Fish Sandwich   $8.99</h3>
+									<p>Blackened tilapia, LTOP w/chipotle mayo on poppy seed bun</p>
+								</li>
 						</div>
 						<div class="col-md-6">
 							<h2>Salads</h2>
-							Served w/Ranch, Bleu Cheese, Balsamic Vinaigrette, Honey Mustard, Thousand Island, Caesar, or Chipotle Ranch
-							Add grilled or crispy chicken $1.99    Add blackened Tilapia $2.99
-							<h3>Dinner Salad  $6.99</h3>
-							Romaine lettuce, mixed cheese, tomato, red onions &amp; croutons
-							<h3>Caesar Salad   $7.29</h3>
-							Crisp romaine lettuce, parmesan cheese, &amp; croutons, tossed in a creamy caesar dressing
-							<h3>Buffalo Chicken Salad   $8.99</h3>
-							Romaine lettuce, mixed cheese, tomato, red onions, bleu cheese crumbles, &amp; crispy buffalo chicken
+							<ul>
+								<li>
+									<p>Served w/Ranch, Bleu Cheese, Balsamic Vinaigrette, Honey Mustard, Thousand Island, Caesar, or Chipotle Ranch<br>
+									Add grilled or crispy chicken $1.99    Add blackened Tilapia $2.99</p>
+								</li>
+								<li>
+									<h3>Dinner Salad  $6.99</h3>
+									<p>Romaine lettuce, mixed cheese, tomato, red onions &amp; croutons</p>
+								</li>
+								<li>
+									<h3>Caesar Salad   $7.29</h3>
+									<p>Crisp romaine lettuce, parmesan cheese, &amp; croutons, tossed in a creamy caesar dressing</p>
+								</li>
+								<li>
+									<h3>Buffalo Chicken Salad   $8.99</h3>
+									<p>Romaine lettuce, mixed cheese, tomato, red onions, bleu cheese crumbles, &amp; crispy buffalo chicken</p>
+								</li>
 						</div>
 					</div> <!-- .row -->
 				</div> <!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu bg-primary">
 				<div class="container">
 					<div class="row">				
 						<div class="col-md-4">
@@ -140,13 +194,13 @@ Template Name: Menu
 						<div class="col-md-8">	
 							
 							<h3>Brownie bottom Sunday $5.29</h3>
-							Warm chocolate brownie covered with vanilla ice cream, whipped cream, caramel &amp; raspberry sauce
+							<p>Warm chocolate brownie covered with vanilla ice cream, whipped cream, caramel &amp; raspberry sauce</p>
 						</div>
 					</div>
 				</div> <!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-6">
@@ -160,14 +214,16 @@ Template Name: Menu
 								<div class="col-md-6">									
 									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu/beer.jpg" class="img-responsive img-circle" alt="Bronson Rock - Wings">
 								</div>
-								<div class="col-md-6">																
-									<h3>Miller Lite</h3>
-									<h3>Coors Light</h3>
-									<h3>Bud Light</h3>
-									<h3>Michelob Ultra</h3>
-									<h3>Dos XX’s Lager</h3>
-									<h3>Sam Adams</h3>
-									<h3>Blue Moon</h3>
+								<div class="col-md-6">
+									<ul>																
+										<li><h3>Miller Lite</h3></li>
+										<li><h3>Coors Light</h3></li>
+										<li><h3>Bud Light</h3></li>
+										<li><h3>Michelob Ultra</h3></li>
+										<li><h3>Dos XX’s Lager</h3></li>
+										<li><h3>Sam Adams</h3></li>
+										<li><h3>Blue Moon</h3></li>
+									</ul>			
 								</div>
 							</div>
 						</div>
@@ -178,21 +234,25 @@ Template Name: Menu
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-md-6">									
-									<h3>Budweiser</h3>
-									<h3>Bud Light</h3>
-									<h3>Miller Lite</h3>
-									<h3>Coors Light</h3>
-									<h3>Michelob Ultra</h3>
-									<h3>Corona</h3>
+								<div class="col-md-6">	
+									<ul>								
+										<li><h3>Budweiser</h3></li>
+										<li><h3>Bud Light</h3></li>
+										<li><h3>Miller Lite</h3></li>
+										<li><h3>Coors Light</h3></li>
+										<li><h3>Michelob Ultra</h3></li>
+										<li><h3>Corona</h3></li>
+									</ul>
 								</div>
 								<div class="col-md-6">
-									<h3>Corona Light</h3>
-									<h3>Red Bridge (Gluten Free)</h3>
-									<h3>Dos XX’s</h3>
-									<h3>Heineken</h3>
-									<h3>Guinness</h3>
-									<h3>Red’s Apple Ale</h3>
+									<ul>
+										<li><h3>Corona Light</h3></li>
+										<li><h3>Red Bridge (Gluten Free)</h3></li>
+										<li><h3>Dos XX’s</h3></li>
+										<li><h3>Heineken</h3></li>
+										<li><h3>Guinness</h3></li>
+										<li><h3>Red’s Apple Ale</h3></li>
+									</ul>
 								</div>								
 							</div>
 						</div>
@@ -200,7 +260,7 @@ Template Name: Menu
 				</div> <!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu bg-primary">
 				<div class="container">
 					<div class="row">			
 						<div class="col-md-6">
@@ -215,18 +275,32 @@ Template Name: Menu
 									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu/drinks2.jpg" class="img-responsive img-circle" alt="Bronson Rock - Signature Cocktails">
 								</div>
 								<div class="col-md-6">
-									<h3>Love Junk</h3>
-									Absolut Vodka, peach schnapps, melon liquor, pineapple juice
-									<h3>B*#CH ON WHEELS</h3>
-									Amaretto, Malibu Rum, Sprite &amp; pineapple juice
-									<h3>Rock Hard Rita (Top Shelf)</h3>
-									Patron Tequila, Gran Marnier, topped off with fresh lime juice
-									<h3>Grape Ape</h3>
-									Blue Curacao, grape vodka, cranberry juice &amp; Sprite
-									<h3>Bronson Bloody Mary</h3>
-									Perfect blend of  vodka, spices, and our signature mix
-									<h3>Mimosa (Sundays only)</h3>
-									Champagne &amp; orange juice
+									<ul>
+										<li>
+											<h3>Love Junk</h3>
+											<p>Absolut Vodka, peach schnapps, melon liquor, pineapple juice</p>
+										</li>
+										<li>
+											<h3>B*#CH ON WHEELS</h3>
+											<p>Amaretto, Malibu Rum, Sprite &amp; pineapple juice</p>
+										</li>
+										<li>
+											<h3>Rock Hard Rita (Top Shelf)</h3>
+											<p>Patron Tequila, Gran Marnier, topped off with fresh lime juice</p>
+										</li>
+										<li>
+											<h3>Grape Ape</h3>
+											<p>Blue Curacao, grape vodka, cranberry juice &amp; Sprite</p>
+										</li>
+										<li>
+											<h3>Bronson Bloody Mary</h3>
+											<p>Perfect blend of  vodka, spices, and our signature mix</p>
+										</li>
+										<li>
+											<h3>Mimosa (Sundays only)</h3>
+											<p>Champagne &amp; orange juice</p>
+										</li>
+									</ul>
 								</div>
 							</div> <!-- .row -->
 						</div> <!-- .col -->
@@ -242,15 +316,16 @@ Template Name: Menu
 									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu/drinks.jpg" class="img-responsive img-circle" alt="Bronson Rock - Frozen Margaritas">
 								</div>								
 								<div class="col-md-6">	
-									<h3>Bronson Frozen Margarita</h3>
-									<h3>Italian Margarita (Amaretto)</h3>
-									<h3>Blue Bayou Margarita (Curacao)</h3>
-									<h3>Peachy Keen Margarita (Peach Schnapps)</h3>
-									<h3>Orange Blossom Margarita (Gran Marnier)</h3>
-									<h3>Spanish Margarita (Sangria)</h3>
-									<h3>Melon Margarita (Melon Liquor)</h3>
-									<h3>Suicide Margarita
-									(All of the Above!)</h3>
+									<ul>
+										<li><h3>Bronson Frozen Margarita</h3></li>
+										<li><h3>Italian Margarita (Amaretto)</h3></li>
+										<li><h3>Blue Bayou Margarita (Curacao)</h3></li>
+										<li><h3>Peachy Keen Margarita (Peach Schnapps)</h3></li>
+										<li><h3>Orange Blossom Margarita (Gran Marnier)</h3></li>
+										<li><h3>Spanish Margarita (Sangria)</h3></li>
+										<li><h3>Melon Margarita (Melon Liquor)</h3></li>
+										<li><h3>Suicide Margarita (All of the Above!)</h3></li>
+									</ul>
 								</div>
 							</div> <!-- .row --> 
 						</div> <!-- .col -->
@@ -258,7 +333,7 @@ Template Name: Menu
 				</div><!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu bg-secondary">
 				<div class="container">
 					<div class="row text-center">	
 						<div class="col-md-12">
@@ -268,32 +343,38 @@ Template Name: Menu
 					<div class="row">							
 						<div class="col-md-4">												
 							<h3>White (Glass)</h3>
-							House Chardonnay
-							House Moscato
-							House Pinot Grigio
-							House White Zinfandel
-							Premium Chardonnay
+							<ul>
+								<li><h3>House Chardonnay</h3></li>
+								<li><h3>House Moscato</h3></li>
+								<li><h3>House Pinot Grigio</h3></li>
+								<li><h3>House White Zinfandel</h3></li>
+								<li><h3>Premium Chardonnay</h3></li>
+							</ul>
 						</div>
 						<div class="col-md-4">	
 							<h3>Red (Glass)</h3>
-							House Cabernet
-							House Merlot
-							Premium Cabernet
-							Premium Merlot
+							<ul>
+								<li><h3>House Cabernet</h3></li>
+								<li><h3>House Merlot</h3></li>
+								<li><h3>Premium Cabernet</h3></li>
+								<li><h3>Premium Merlot</h3></li>
+							</ul>
 						</div>
 						<div class="col-md-4">
 							<h3>Bottle</h3>
-							J. Lohr Merlot
-							Casillero Del Diablo
-							Cabernet
-							Josh Chardonnay
-							* ask your server for details on wine
+							<ul>
+								<li><h3>J. Lohr Merlot</h3></li>
+								<li><h3>Casillero Del Diablo</h3></li>
+								<li><h3>Cabernet</h3></li>
+								<li><h3>Josh Chardonnay</h3></li>
+								<li><h3><p>* ask your server for details on wine</p></h3></li>
+							</ul>
 						</div>		
 					</div>
 				</div> <!-- .container -->
 			</section>
 
-			<section class="menu bg-secondary">
+			<section class="section menu bg-secondary">
 				<div class="container">
 					<div class="row">	
 						<div class="col-md-6">	
