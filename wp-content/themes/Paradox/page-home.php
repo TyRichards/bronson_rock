39a3ffd0ed06dbcc45dc2b506bc44d8f98ac963e
Ty Richards <me@tyrichards.com>
2015-03-25 13:46:49 -0500

    restore date

--- wp-content/themes/Paradox/page-home.php
+++ wp-content/themes/Paradox/page-home.php
@@ -42,7 +42,7 @@ Template Name: Home
                 		?>   
                 		
                 		<a class="media" href="http://<?php the_field('url'); ?>" target="_blank">    
-                        <!-- <span class="pull-left"><?php // echo $date->format('D M j'); ?></span>-->
+                        <span class="pull-left"><?php echo $date->format('D M j'); ?></span>
                         <div class="media-body">
                             <h2><?php the_field('event_title'); ?></h2>
                             <p class="details"><?php the_field('event_time'); ?> | View Details</p>
